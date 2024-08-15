30aca9d473750620e21b9c2d076bc6783a7c11c1
S-hira <ahmedabduelmalik6@gmail.com>
2024-08-15 18:33:23 +0300

    auth2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+//import { initializeApp } from "firebase/app";
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,13 +15,15 @@ const firebaseConfig = {
   messagingSenderId: "94422243821",
   appId: "1:94422243821:web:a274bee2c61bcd6bd6afe4"
 };
-
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const auth = firebase.auth(app);
+//const auth = firebase.auth(app);
+const auth = getAuth(app);
+
 
     // Handle Sign Up
-    document.getElementById('signInForm').addEventListener('submit', (event) => {
+    /*document.getElementById('signInForm').addEventListener('submit', (event) => {
       event.preventDefault();
       
       const email = document.getElementById('email').value;
@@ -38,4 +42,4 @@ const auth = firebase.auth(app);
           console.error('Error during sign-up:', errorCode, errorMessage);
           document.getElementById('error-message').textContent = errorMessage;
         });
-    });
+    });*/
